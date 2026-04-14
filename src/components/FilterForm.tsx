9f857e999ea73e24fb7b42399fc54d69cb74bc17
Jeno Csupor <jeno.csupor@endava.com>
2026-04-14 15:23:40 +0300

    Change to 500 to don't get distracted when typing space

--- src/components/FilterForm.tsx
+++ src/components/FilterForm.tsx
@@ -21,7 +21,7 @@ export function FilterForm({
       if (localQuery !== query) {
         setQuery(localQuery);
       }
-    }, 300);
+    }, 500);
 
     return () => clearTimeout(handler);
   }, [localQuery, setQuery, query]);
