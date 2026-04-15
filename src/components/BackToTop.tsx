9471cca027c3a4d88c4e9578ae75c91fed8c4d14
Jeno Csupor <jeno.csupor@endava.com>
2026-04-15 15:40:08 +0300

    Use classes for back to top spacing

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -12,23 +12,21 @@ export const BackToTop = ({
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  const opacityClass = isActive ? "opacity-100" : "opacity-0";
-  const buttonTransform = isActive ? "translateY(0)" : "translateY(20px)";
-  const buttonPointerEvents = isActive ? "auto" : "none";
+  const opacity = isActive ? "opacity-100" : "opacity-0";
+  const transform = isActive ? "translateY(0)" : "translateY(20px)";
+  const pointerEvents = isActive ? "auto" : "none";
 
   return (
     <Button
       onClick={scrollToTop}
-      className={`rounded-2 shadow-sm position-fixed border-0 d-flex align-items-center justify-content-center bg-white text-black ${opacityClass}`}
+      className={`rounded-2 shadow-sm position-fixed bottom-0 end-0 mb-2 me-1 border-0 d-flex align-items-center justify-content-center bg-white text-black ${opacity}`}
       style={{
-        bottom: "8px",
-        right: "4px",
         zIndex: 9999,
         width: "60px",
         height: "60px",
         transition: "all 0.2s ease-in-out",
-        transform: buttonTransform,
-        pointerEvents: buttonPointerEvents,
+        transform: transform,
+        pointerEvents: pointerEvents,
       }}
     >
       <svg
