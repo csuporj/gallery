a7e4a8fb241f88e5e296c803ea7ce154a40991dd
Jeno Csupor <jeno.csupor@endava.com>
2026-04-21 11:43:28 +0300

    try again

--- src/components/useAlbumParams.ts
+++ src/components/useAlbumParams.ts
@@ -1,26 +1,26 @@
-import type { DateState } from "./types";
+// import type { DateState } from "./types";
 
-import { useCallback, useMemo } from "react";
-import { useSearchParams } from "react-router-dom";
+// import { useCallback, useMemo } from "react";
+// import { useSearchParams } from "react-router-dom";
 
-function parseDateFilter(params: URLSearchParams): DateState {
-  return {
-    y: params.get("y") ?? "*",
-    m: params.get("m") ?? "*",
-    d: params.get("d") ?? "*",
-  };
-}
+// function parseDateFilter(params: URLSearchParams): DateState {
+//   return {
+//     y: params.get("y") ?? "*",
+//     m: params.get("m") ?? "*",
+//     d: params.get("d") ?? "*",
+//   };
+// }
 
-function buildSearchParams(query: string, date: DateState): URLSearchParams {
-  const params = new URLSearchParams();
+// function buildSearchParams(query: string, date: DateState): URLSearchParams {
+//   const params = new URLSearchParams();
 
-  if (query) params.set("q", query);
-  if (date.y !== "*") params.set("y", date.y);
-  if (date.m !== "*") params.set("m", date.m);
-  if (date.d !== "*") params.set("d", date.d);
+//   if (query) params.set("q", query);
+//   if (date.y !== "*") params.set("y", date.y);
+//   if (date.m !== "*") params.set("m", date.m);
+//   if (date.d !== "*") params.set("d", date.d);
 
-  return params;
-}
+//   return params;
+// }
 
 export function useAlbumParams() {
   // const [searchParams, setSearchParams] = useSearchParams();
