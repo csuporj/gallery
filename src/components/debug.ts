3af064af7cb5de522607f8017a47df3e58c90e72
Jeno Csupor <jeno.csupor@endava.com>
2026-04-21 15:34:41 +0300

    remove :

--- src/components/debug.ts
+++ src/components/debug.ts
@@ -8,5 +8,5 @@ export function getTimestamp(): string {
   const ss = d.getSeconds().toString().padStart(2, "0");
   const ms = d.getMilliseconds().toString().padStart(3, "0");
 
-  return "[" + hh + ":" + mm + ":" + ss + "." + ms + "]:";
+  return "[" + hh + ":" + mm + ":" + ss + "." + ms + "]";
 }
